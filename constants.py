f86cdfb4b674b7105e542b02c3831dfb786a4fea
Tural Gurbanov <madcat1991@gmail.com>
2014-01-12 15:20:46 +0400

    Первичная подготовка к новому скрипту

---
+++ constants.py
@@ -0,0 +1,66 @@
+#coding: utf-8
+
+NOT_AVAILABLE = "n/a"
+UNMATCHED = "unmatched"
+CURRENT_DIRECTORY = "/home/tural/not_univer/experiments/brat_statistics/sources"
+ALLOWED_MISTAKES = [
+    "Content",
+        "content_reference",
+            "omission",
+            "non-sense",
+        "content_cohesion",
+            "theme",
+            "rheme",
+            "connector",
+            "logic",
+        "content_pragmatics",
+            "register",
+            "use",
+    "Language",
+    "Language_lexical",
+        "lexical",
+        "combinability",
+    "Language_morphology",
+    "Language_syntax",
+        "incomplete_structure",
+        "ungrammatical",
+        "word_order",
+    "Laguage_spelling",
+        "capitals",
+    "Language_punctuation",
+    "Delete",
+    "All:",
+    "Content:",
+    "Language:"
+]
+
+LANG_MISTAKES = [
+    "Language",
+    "Language_lexical",
+        "lexical",
+        "combinability",
+    "Language_morphology",
+    "Language_syntax",
+        "incomplete_structure",
+        "ungrammatical",
+        "word_order",
+    "Laguage_spelling",
+        "capitals",
+    "Language_punctuation",
+    "Delete"
+]
+
+CONTENT_MISTAKES = [
+    "Content",
+        "content_reference",
+            "omission",
+            "non-sense",
+        "content_cohesion",
+            "theme",
+            "rheme",
+            "connector",
+            "logic",
+        "content_pragmatics",
+            "register",
+            "use"
+]
